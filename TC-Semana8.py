a84d282f487145ea94ff607788ec917e2b6d25c6
Irsacfac <irsacfac@gmail.com>
2020-06-02 15:30:57 -0600

    Problema de n reinas resuelto con LasVegas
    Algunas veces resuelve el problema con exito otras veces reorna vacio

--- TC-Semana8.py
+++ TC-Semana8.py
@@ -24,28 +24,43 @@ def swap(A, x, y):
     A[y] = temp
 
 a = [0,1,2,3,4]
+e = (N+1) * [True]
 queens(a, 4, N-1)
 
 
-def queensVegas(Array, rRestanets, x):
+def queensVegas(reinas):
+    encontrado = False
+    contador = 0
+    while(not encontrado):
+        encontrado = auxQueensVegas([], reinas, reinas-1)
+
+def auxQueensVegas(Array, rRestantes, x):
     if rRestantes==0:
-        print (A)
+        print (Array)
+        return True
     else:
-        for i in range (rRestates, 0, -1):
-            colDisponibles = columnasDisponibles(Array)
-            if(len(colDisponibles)==0):
-                return
+        colDisponibles = columnasDisponibles(e, rRestantes, x)
+        if(len(colDisponibles)==0):
+            return False
+        col = colDisponibles[random.randint(0, len(colDisponibles)-1)]
+        e[col] = False
+        b[col+rRestantes] = False
+        d[(col-rRestantes)+x] = False
+        Array.append(col)
+        auxQueensVegas(Array, rRestantes-1, x)
             
-def columnasDIsponibles(Array, fila, x):
+def columnasDisponibles(Array, fila, x):
     disponibles = []
     for columna in range(len(Array)-1, 0, -1):
-        if(Array[columna] and b[columna+fila]):
+        if(Array[columna] and b[columna+fila] and d[(columna-fila)+x]):
             disponibles.append(columna)
     return disponibles
     
 def cambio(a):
     a = not a
     return a
+
+queensVegas(4)
 
 
 def cicloFor(x,n):
