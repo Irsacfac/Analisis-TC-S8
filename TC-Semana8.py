028f893eb41821c6ffd325732027edb7e671b3fd
Irsacfac <irsacfac@gmail.com>
2020-06-04 14:11:08 -0600

    Se solucionó el problema
    Ahora, generalmente, devuelve una respuesta

--- TC-Semana8.py
+++ TC-Semana8.py
@@ -15,16 +15,19 @@ def auxQueens(A, m, x, T):
         tFinal = time()
         tEjecucion = tFinal - T
         print("El tiempo de ejecución backtracking fue: ", tEjecucion)
+        return True
     else:
         for i in range (m, 0, -1):
             if (b[A[i]+m] and d[(A[i]-m)+x]):
                 b[A[i]+m]=False
                 d[(A[i]-m)+x]=False
                 swap(A, m, i)
-                queens(A, m-1, x)
+                if(auxQueens(A, m-1, x, T)):
+                    return True
                 swap(A, m, i)
                 b[A[i]+m]=True
                 d[(A[i]-m)+x]=True
+        return False
 
 def swap(A, x, y):
     temp = A[x]
@@ -37,11 +40,15 @@ def queensVegas(reinas):
     encontrado = False
     contador = 0
     tInicial = time()
-    while(not encontrado and contador<=(reinas*reinas)):
-        encontrado = auxQueensVegas([], reinas, reinas-1, tInicial)
+    while(not encontrado):
+        B = ((2*N)+1) * [True]
+        D = ((2*N)-1) * [True]
+        E = (N+1) * [True]
+        encontrado = auxQueensVegas([], reinas, reinas-1, B, D, E, tInicial)
+        contador+=1
     return
 
-def auxQueensVegas(Array, rRestantes, x, T):
+def auxQueensVegas(Array, rRestantes, x, B, D, E, T):
     if rRestantes==0:
         print (Array)
         tFinal = time()
@@ -49,20 +56,20 @@ def auxQueensVegas(Array, rRestantes, x, T):
         print("El tiempo de ejecución LasVegas fue: ", tEjecucion)
         return True
     else:
-        colDisponibles = columnasDisponibles(e, rRestantes, x)
+        colDisponibles = columnasDisponibles(E, rRestantes, x, B, D, E,)
         if(len(colDisponibles)==0):
             return False
         col = colDisponibles[random.randint(0, len(colDisponibles)-1)]
-        e[col] = False
-        b[col+rRestantes] = False
-        d[(col-rRestantes)+x] = False
+        E[col] = False
+        B[col+rRestantes] = False
+        D[(col-rRestantes)+x] = False
         Array.append(col)
-        return auxQueensVegas(Array, rRestantes-1, x, T)
+        return auxQueensVegas(Array, rRestantes-1, x, B, D, E, T)
             
-def columnasDisponibles(Array, fila, x):
+def columnasDisponibles(Array, fila, x, B, D, E,):
     disponibles = []
     for columna in range(len(Array)-1, 0, -1):
-        if(Array[columna] and b[columna+fila] and d[(columna-fila)+x]):
+        if(Array[columna] and B[columna+fila] and D[(columna-fila)+x]):
             disponibles.append(columna)
     return disponibles
 
